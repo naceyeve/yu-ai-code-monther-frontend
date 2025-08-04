0fe9671c4c3a03dd92eb6f779da33718887f789d
penngli <271948021@qq.com>
2025-08-04 17:04:03 +0800

    引入Ant Design组件库

--- yu-ai-code-monther-frontend/src/main.ts
+++ yu-ai-code-monther-frontend/src/main.ts
@@ -3,10 +3,11 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import 'ant-design-vue/dist/reset.css'
+import Antd from 'ant-design-vue'
 const app = createApp(App)
-
 app.use(createPinia())
 app.use(router)
+app.use(Antd)
 
 app.mount('#app')
